eb8cfaa8a4ea9b24ba6eeaa68f61570a0371c1ce
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-05-02 10:23:07 +0400

    Refactoring: switching from JSONbin to custom server

--- src/api/itemsAPI.ts
+++ src/api/itemsAPI.ts
@@ -1,24 +1,19 @@
 import {dataFetch} from "./api";
 import {Item} from "../types/Item";
-import {JSONbinResponse} from "../types/JSONbinResponse";
 
-// Localhost server
-let localDB = dataFetch('http://localhost:3004')
-
-// JSON bin server
-const jsonbinAPI = dataFetch('https://api.jsonbin.io/v3/b')
+let serverAPI = dataFetch('http://localhost:3004')
 
 // Functions for localhost server
 export let load_items = async (): Promise<Item[]> => {
-    return await localDB<Item[]>('items');
+    return await serverAPI<Item[]>('items_simple');
 }
 
 export let load_item_by_id = async (id: string): Promise<Item> => {
-    return await localDB<Item>(`items/${id}`);
+    return await serverAPI<Item>(`items_simple/${id}`);
 }
 
 export let post_item = async (item: Item): Promise<Item> => {
-    return await localDB<Item>('items', {
+    return await serverAPI<Item>('items_simple', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -29,13 +24,14 @@ export let post_item = async (item: Item): Promise<Item> => {
 
 export let delete_item = async (id: string): Promise<{}> => {
 
-    return await localDB<{}>('items/' + id, {
+    return await serverAPI<{}>('items_simple/' + id, {
         method: 'DELETE'
     })
 }
 
 export let update_item = async (id: string, item: Item): Promise<Item> => {
-    return await localDB<Item>('items/' + id, {
+
+    return await serverAPI<Item>('items_simple/' + id, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
@@ -44,10 +40,9 @@ export let update_item = async (id: string, item: Item): Promise<Item> => {
     })
 }
 
-// If Localhost server is unavailable, then the functions are replaced to fetch data from JSONbin
 export const change_API_path = (): void => {
     const warning_text: HTMLParagraphElement = document.createElement('p')
-    warning_text.innerText = "Using JSONbin API to store tasks. \n It may take time to fetch data."
+    warning_text.innerText = "Fetching data from remote server"
     warning_text.style.opacity = "0.3"
     warning_text.style.fontSize = "12px";
     warning_text.style.position = "fixed";
@@ -57,73 +52,5 @@ export const change_API_path = (): void => {
 
     document.body.append(warning_text)
 
-    load_items = async (): Promise<Item[]> => {
-        return await jsonbinAPI<JSONbinResponse>('643d4670ace6f33a220cf2db', {
-            method: 'GET',
-            headers: {
-                'X-Master-Key': '$2b$10$KaHvykHsLNyRLB/SubZcF.j3TnmR./yJ5VVyqOcikmTeBJ6BTBeEK'
-            }
-        }).then((response: JSONbinResponse) => response.record.items)
-    }
-
-    load_item_by_id = async (id: string): Promise<Item> => {
-        return await jsonbinAPI<JSONbinResponse>('643d4670ace6f33a220cf2db', {
-            method: 'GET',
-            headers: {
-                'X-Master-Key': '$2b$10$KaHvykHsLNyRLB/SubZcF.j3TnmR./yJ5VVyqOcikmTeBJ6BTBeEK'
-            }
-        }).then((response: JSONbinResponse) => {
-            const index: number = response.record.items.findIndex((item: Item): boolean => item.id === id)
-            return response.record.items[index]
-        })
-    }
-
-    post_item = async (item: Item): Promise<Item> => {
-
-        const items: Item[] = await load_items()
-
-        return await jsonbinAPI<JSONbinResponse>('643d4670ace6f33a220cf2db', {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Master-Key': '$2b$10$KaHvykHsLNyRLB/SubZcF.j3TnmR./yJ5VVyqOcikmTeBJ6BTBeEK'
-            },
-            body: JSON.stringify({items: [...items, item]})
-        }).then(() => item)
-    }
-
-    delete_item = async (id: string): Promise<{}> => {
-
-        let items: Item[] = await load_items()
-
-        items = items.filter((item: Item): boolean => item.id !== id)
-
-        return await jsonbinAPI<JSONbinResponse>('643d4670ace6f33a220cf2db', {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Master-Key': '$2b$10$KaHvykHsLNyRLB/SubZcF.j3TnmR./yJ5VVyqOcikmTeBJ6BTBeEK'
-            },
-            body: JSON.stringify({items: [...items]})
-        }).then(() => ({}))
-    }
-
-    update_item = async (id: string, item: Item): Promise<Item> => {
-
-        let items: Item[] = await load_items()
-
-        items = items.map((task: Item): Item => task.id === id ? item : task)
-
-        return await jsonbinAPI<JSONbinResponse>('643d4670ace6f33a220cf2db', {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Master-Key': '$2b$10$KaHvykHsLNyRLB/SubZcF.j3TnmR./yJ5VVyqOcikmTeBJ6BTBeEK'
-            },
-            body: JSON.stringify({items: [...items]})
-        }).then((response: JSONbinResponse) => {
-            const index: number = response.record.items.findIndex((item: Item): boolean => item.id === id)
-            return response.record.items[index]
-        })
-    }
+    serverAPI = dataFetch('https://brainy-hem-lion.cyclic.app')
 }
