5380c8d0b065987f1d052b1861a7afd872c1ea31
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-05-06 11:02:40 +0400

    Feature: syncing Redux Store between tabs using State-Sync middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,20 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {itemReducer} from "./slices/itemSlice";
 import {weatherReducer} from "./slices/weatherSlice";
+import {createStateSyncMiddleware, initStateWithPrevTab, withReduxStateSync} from "redux-state-sync";
 
-const reducer = combineReducers({
+const reducer = withReduxStateSync(combineReducers({
     items: itemReducer,
     weather: weatherReducer
-})
+}))
 
 export const store = configureStore({
-    reducer
+    reducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(createStateSyncMiddleware()),
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+
+initStateWithPrevTab(store);
